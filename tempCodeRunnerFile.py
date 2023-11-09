219d08582ab4faf751fa516e591bc201fb8ce991
rajsrm2021 <rj9769@srmist.edu.in>
2023-11-09 22:26:40 +0530

    init

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,2 @@
-import pandas as pd
-
-df = pd.read_excel(
-    io="Dataset/supermarkt_sales.xlsx",
-    engine="openpyxl",
-    sheet_name="Sales",
-    skiprows=3,
-    usecols="B:R",
-    nrows=1000,
-)
-
-print(df)
+as px
+import streamlit as st
